fb7ad4d59603d1b39fee0b8d6b445d88fb8741b9
przeuas <przeus13@o2.pl>
2018-09-19 13:59:01 +0200

    mini blog

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,61 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.http import HttpResponse
-
+from .models import Post
 from .models import Greeting
-
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request,'index.html', {'posts': posts})
 
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+#    post.publish() - usuniete, bo domysle ze strony nie działało
+    post.published_date = timezone.now()
+    post.save();
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('index')
 
 def db(request):
 
@@ -17,4 +65,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
